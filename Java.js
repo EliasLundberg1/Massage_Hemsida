334c737dfdda842c2dfcce883434cdf16df06d73
Elias Lundberg <elilun060126@student.jenseneducation.se>
2023-11-28 11:08:06 +0100

    First Commit

---
+++ Java.js
@@ -0,0 +1,13 @@
+function changeColor() 
+/*gör så att när man trycker på detta element så byter den färgen på texten till vit*/
+{
+    var element = document.getElementById("changeColorElement");
+    element.style.color = "#FFFFFF"; 
+}
+
+function changeBackgroundColor()
+/*gör så att när man trycker på detta element så byter den färgen på bakgrunden till svart*/
+{
+    var element = document.getElementById("changeBackgroundColorElement");
+    element.style.backgroundColor = "#000000"
+}
